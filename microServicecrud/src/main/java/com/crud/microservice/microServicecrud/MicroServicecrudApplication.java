a1b0c1b570dcf43fd959811c16e64e0a72c85fe3
Subham Kr Gupta <subham.gupta@motivitylabs.com>
2023-02-27 14:21:59 +0530

    Add Project

---
+++ microServicecrud/src/main/java/com/crud/microservice/microServicecrud/MicroServicecrudApplication.java
@@ -0,0 +1,13 @@
+package com.crud.microservice.microServicecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicecrudApplication.class, args);
+	}
+
+}
---
+++ microServicecrud/src/test/java/com/crud/microservice/microServicecrud/MicroServicecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.microservice.microServicecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
